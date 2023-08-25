ba312dc00fedb1d6356c2c8d1e3c8e8d67dcb163
Divyansh Srivastavae <divyanshsrivastava215@gmail.com>
2023-08-25 17:04:43 +0530

    new file:   .env.example 	new file:   .gitignore 	new file:   config/db.js 	new file:   models/file.js 	new file:   package-lock.json 	new file:   package.json 	new file:   public/apple-icon.png 	new file:   public/cover.png 	new file:   public/css/style.css 	new file:   public/css/style.min.css 	new file:   public/favicon.ico 	new file:   public/favicon.png 	new file:   public/img/copy.svg 	new file:   public/img/file.png 	new file:   public/img/mail.svg 	new file:   public/js/app.js 	new file:   routes/cleanup.js 	new file:   routes/download.js 	new file:   routes/filePreview.js 	new file:   routes/files.js 	new file:   routes/home.js 	new file:   routes/listing.js 	new file:   script.js 	new file:   server.js 	new file:   services/emailService.js 	new file:   services/emailTemplate.js 	new file:   uploads/.gitkeep 	new file:   uploads/1692914620362-54072664.jpg 	new file:   uploads/1692914678449-92877824.jpg 	new file:   uploads/1692914703405-350372312.jpg 	new file:   views/cleanup.ejs 	new file:   views/download.ejs 	new file:   views/home.ejs 	new file:   views/listing.ejs 	new file:   yarn.lock

---
+++ config/db.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+function connectDB() {
+  const connectionString = process.env.MONGODB_CONNECTION_URL;
+
+  mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+
+  const connection = mongoose.connection;
+
+  connection.on('error', (error) => {
+    console.error('Database connection failed', error);
+    process.exit(1);
+  });
+
+  connection.once('open', () => {
+    console.log('Database connected');
+  });
+}
+
+module.exports = connectDB;
